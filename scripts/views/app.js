733704822bfe8ff10c47a4878f6fe0e5bd6b37b0
Anton Ball <antonball@gmail.com>
2012-09-09 23:13:38 +0800

    Presentation version.

--- scripts/views/app.js
+++ scripts/views/app.js
@@ -2,26 +2,35 @@ var AppView = Backbone.View.extend({
 
     initialize: function (options) {
         this.model.on('change', this.render, this);
-        this.createViews();
     },
 
     render: function () {
         var self = this;
-        dust.render('app', {}, function (err, out) {
-            self.$el.appendTo('body');
+
+        dust.render('app-template', {}, function (err, out) {
+            self.$el.attr('role', 'main');
+            self.$el.append(out).appendTo('body');
+            self.createViews();
+            self.views.dreams.render();
+            self.views.addView.render();
         });
+
+        return this;
     },
 
     createViews: function () {
         // Create and render all child views
         this.views = {
             dreams: new DreamsView({
-                el: $("#dreams-todo"),
+                el: $(".container"),
                 collection: this.model.dreams
             }),
             profile: new ProfileView({
                 el: $("#summary"),
                 model: this.model // user model
+            }),
+            addView: new AddView({
+                el: $('.container')
             })
         };
     }
--- scripts/views/dreams.js
+++ scripts/views/dreams.js
@@ -1,8 +1,20 @@
 var DreamsView = Backbone.View.extend({
 
+    dreamClass: 'dream-item',
+    activeClass: 'active',
+
     // Delegate events hash
     events: {
-        'button.save': 'save'
+        'button.save': 'save',
+        'click .dream-item': 'toggleView'
+    },
+
+    toggleView: function(ev){
+        var $ct = $(ev.currentTarget),
+            selected = $ct.hasClass(this.activeClass);
+
+        $('.' + this.activeClass, this.$el).removeClass(this.activeClass);
+        if (!selected) $ct.addClass(this.activeClass);
     },
 
     initialize: function () {
@@ -17,10 +29,18 @@ var DreamsView = Backbone.View.extend({
     },
 
     render: function () {
-        var self = this;
+        var self = this,
+            data = {
+                dreams: this.collection.toJSON()
+            };
 
-        dust.render('list-template', this.collection.toJSON(), function (err, out) {
+        dust.render('list-item-template', data, function (err, out) {
             self.$el.append(out);
+            self.$el.find('.time').each(function(index, el){
+                var $this = $(el),
+                    when = moment(new Date(Number($this.text())));
+                $this.text(when.format("MMM, YYYY"));
+            });
         });
     }
 
